6913717f0344cadfd1d7ff51e4b3094948247a82
Aaron <parisi.aaron@gmail.com>
2021-06-03 03:44:14 +0000

    about section formatting

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Helmet } from 'react-helmet'
+import Headshot from "../../images/headshot.png"
 
 const About = props => {
   return (
-    <div className="about" id="about" >
-      <h1>About</h1>
+    <div className="about" id="about" data-dark={true}>
+        <header className="about-header">
+          <h1>Aaron Parisi</h1>
+          <h2>I'm a web developer based in Seattle, WA,</h2>
+          <h2>specializing in Rails + React development.</h2>
+        </header>
 
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque sequi, sit ab in molestiae dolor, tempore dolorem molestias debitis nihil deleniti minus nam eum quasi laborum asperiores nobis earum quam?</p>
+        <div className="about-content" id="about-content">
+          <div className="about-text">
+            <p>
+              It started when I was teaching AP Calculus BC, Multivariable Calculus, and Statistics at <a href="https://www.salisburyschool.org/">Salisbury School</a> on a whiteboard...
+            </p>
+            <p>
+              Then I worked a job at a <a href="https://www.1031services.com/">real estate exchange company</a> whose database software had some... quirks...
+            </p>
+            <p>
+              Finally it dawned on me - I want to make web applications that're easy to use, that actually work, and that *gasp* look nice.  With the help of <a href="https://open.appacademy.io/">App Academy Open</a>,
+              I've been able to teach myself to do that.
+            </p>
+          </div>
+          <div className="headshot-container"><img id="headshot" src={Headshot} alt="Headshot of Aaron Parisi"/></div>
+        </div>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { debounce } from '../../utils/helpers';
+
+const inFrontOfDark = () => {
+  const ele = document.elementFromPoint(0,75)
+  
+  if (ele.dataset.dark === undefined) {
+    return false
+  } else {
+    return ele.dataset.dark === "true"
+  }
+}
 
 const NavBar = props => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [lightNav, setLightNav] = useState(inFrontOfDark())
+  const [visible, setVisible] = useState(true);
+
+
+  useEffect(() => {
+    return () => setLightNav(inFrontOfDark())
+  }, [props])
+  
+  useEffect(() => {
+    let isScrolling;
+
+    const handleScroll = debounce(() => {
+      window.clearTimeout(isScrolling)
+
+      isScrolling = setTimeout(() => {
+        adjustState();
+      }, 100);
+    }, 250)
+
+    const adjustState = () => {
+      const curScrollPos = window.pageYOffset
+  
+      setVisible(  // scrolling UP or at top
+        (prevScrollPos > curScrollPos) ||
+        curScrollPos < 10
+      )
+
+      setTimeout(() => {
+        setLightNav(inFrontOfDark())
+      }, 1000);
+      setPrevScrollPos(curScrollPos)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [prevScrollPos, visible])
+
+  const navBarStyles = {
+    position: 'fixed',
+    left: '0px',
+    height: '75px',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    zIndex: '3',
+    transition: 'top 0.3s, background-color 0.3s, box-shadow 0.3s',
+    top: visible ? '0px' : '-100px',
+    backgroundColor: lightNav ? '#000C40' : 'transparent',
+    borderBottom: lightNav ? '1px solid #ffffff' : 'none',
+    boxShadow: lightNav ? '0px 1px 10px #000000' : 'none'
+  }
 
   const SessionLinks = () => {
     // debugger
@@ -34,21 +98,23 @@ const NavBar = props => {
   }
 
   return (
-    <nav>
-      <ul className="nav-links">
-        <li className="nav-link">
-          <a href="#about">About</a>
-        </li>
-        <li className="nav-link">
-          <a href="#projects">Projects</a>
-        </li>
-        <li className="nav-link">
-          <a href="#contact">Contact</a>
-        </li>
+    <div className="nav-container" style={{...navBarStyles}}>
+      <nav>
+        <ul className="nav-links">
+          <li className="nav-link">
+            <a href="#about">About</a>
+          </li>
+          <li className="nav-link">
+            <a href="#projects">Projects</a>
+          </li>
+          <li className="nav-link">
+            <a href="#contact">Contact</a>
+          </li>
 
-        {/* <SessionLinks /> */}
-      </ul>
-    </nav>
+          {/* <SessionLinks /> */}
+        </ul>
+      </nav>
+    </div>
   )
 }
 
