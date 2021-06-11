5b84f20f9d5d1f49b44557e3322390717a791e09
Aaron <parisi.aaron@gmail.com>
2021-06-11 03:56:15 +0000

    terminal applet

--- src/components/Projects/BlankTile.jsx
+++ src/components/Projects/BlankTile.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components';
+
+const StyledSpan = styled.span`
+  visibility: ${ ({ visibility }) => visibility};
+  margin-left: ${ ({ margin }) => margin }
+`
+
+const fontWidth = 15;
 
 const BlankTile = props => {
+  const [termText, setTermText] = useState('')
+  const [commands, setCommands] = useState([])
+
+  const handleText = e => {
+    e.preventDefault()
+
+    setTermText(e.currentTarget.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    setCommands([...commands, termText])
+
+    setTermText('')
+  }
 
   return (
     <div className="blank-tile grid-tile">
       <div className="terminal-text">
-        $<span id="blinking-cursor">&#9646;</span>
+        <form
+          onSubmit={e => handleSubmit(e)}
+        >
+          <div className="cmdPrompt">$</div>
+          <input
+            type="text"
+            id="cmd-input"
+            value={termText}
+            placeholder="sudo rm -rf"
+            onChange={e => handleText(e)}
+          />
+        </form>
+        <ul className="prev-commands">
+          {
+            commands.map((cmd, idx) => {
+              return (
+                <li className="cmd" id={idx}>
+                  <div className="cmdPrompt">$</div>{cmd}
+                </li>
+              )
+            })
+          }
+        </ul>
+        {/* <StyledSpan visibility={cursorVisibility} margin={cursorLeftMargin} id="blinking-cursor">&#9646;</StyledSpan> */}
       </div>
     </div>
   )
