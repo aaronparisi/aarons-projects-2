c836f19b6c9a9da1de6da315b531602912f4164d
aaronparisi <parisi.aaron@gmail.com>
2022-04-17 19:56:45 -0700

    added frontend sandbox

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,10 +12,20 @@ import Candie from "../../thumbnails/candie.png";
 import Jeerkat from "../../thumbnails/jeerkat.png";
 import Minesweeper from "../../thumbnails/minesweeper.png";
 import Portfolio1 from "../../thumbnails/portfolio1.png";
+import FAQ from "../../thumbnails/faq.png";
 
 const Projects = (props) => {
   return (
     <div className="projects" id="projects" data-nav="show">
+      <ProjectTileContainer
+        title="Frontend Sandbox"
+        imageUrl={FAQ}
+        pageUrl="https://www.frontend-sandbox.aaronparisi.dev"
+        gitUrl="https://github.com/aaronparisi/frontend-sandbox"
+        description="My most enjoyable set of projects 🎨"
+        techs={["React", "TypeScript"]}
+        underConstruction={true}
+      />
       <ProjectTileContainer
         title="Coupon Vision"
         imageUrl={Coupons}
